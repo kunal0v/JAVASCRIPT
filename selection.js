a10a1ae2d80b6436043b7ebdd61c0d028a27aa3c
kunal0v <bhandarikunal26@gmail.com>
2024-08-27 17:54:52 +0530

    selection

---
+++ selection.js
@@ -0,0 +1,5 @@
+var c=document.querySelector("h1");
+// c.textContent="hellllllllo bacho";
+c.textContent="<i> Kunal Bhandari <i>";
+c.innerHTML="<i> Kunal Bhandari <i>";
+c.style.color="red";
--- selection_Event.js
+++ selection_Event.js
@@ -4,13 +4,14 @@ console.log(document.getElementById("btn"))
 // console.log(document.getElementsByClassName("class"))
 console.log(document.getElementsByTagName("h1"))
 
-// textContent
+
+// textContent=> used for modification
 var btn = document.querySelector("h1")
 btn.textContent ="hello javascript.."
 btn.textContent += "starting.."
+console.log(<i>"Hello world"</i>)
 
-
-innerHTML
+// innerHTML
 var h1 =document.querySelector("h1")
 h1.textContent ="<i>hello</i>"
 h1.innerHTML="<i>hello</i>"
