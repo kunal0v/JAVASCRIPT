0f472580d9d7ab6d9c7349df67a728394f360704
kunal0v <bhandarikunal26@gmail.com>
2024-09-07 16:24:24 +0530

    bounce

---
+++ DOM/bounce.js
@@ -0,0 +1,18 @@
+gsap.from(".parent1 #child1",{
+    scale:0,
+    duration:1,
+    delay:1
+})
+
+gsap.from(".parent2 #child2",{
+    scale:0,
+    duration:1,
+    delay:1
+})
+
+gsap.from(".parent3 #child3",{
+    scale:0,
+    duration:1,
+    delay:1
+
+})
