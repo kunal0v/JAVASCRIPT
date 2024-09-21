3a86b345cb11720fdd5cff011a9bb17f50eae64d
kunal0v <bhandarikunal26@gmail.com>
2024-09-21 12:46:08 +0530

    java revise

---
+++ DOM/GSAP/pract.js
@@ -0,0 +1,21 @@
+// const a="kunal"
+// var b="ru"
+// // var a="dam"
+// let c ="78"
+// console.log(a+b)
+
+
+// hoisting: variable baad main define kia hh but use pehle karr sakte hh 
+// : variable and functions are hoisted which means theri declaration is moved on the top of
+
+// var a;
+// console.log(a);
+// var a=23;
+// we get undefined in replace of error
+// undefined->exist krta hh but define ni kia hh
+// not defined-> no existence
+
+// Primitive and reference
+//Primitive: number, string, null, undefined , boolean, 
+// Reference: [], {} and (), aese values
+
